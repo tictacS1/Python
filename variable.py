1261ed3e5140ebbde36b34370f2ef76bc08fa003
[tictacS1] <[malick.gdy@gmail.com]>
2023-06-05 19:11:33 +0200

    first files

---
+++ variable.py
@@ -0,0 +1,50 @@
+#Integer / NombreEntier
+number1 = 123
+number1 = 42
+print(number1)
+
+#Nombre a virgule/ float
+number2 = 3.14
+number2 = 2.71
+print(number2)
+
+#Chaine de char / txt
+text = "Salut a toutes"
+print(text)
+
+text2 = "et a tous"
+print(text2)
+
+#booleen / boolean
+python_is_cool = True
+print(python_is_cool)
+
+python_is_not_cool = False
+print(python_is_not_cool)
+
+#nul / null
+accepted_terms = None
+print(None)
+
+#type de variables
+print(type(text))
+
+#verif du type de données
+print(type (text2))
+
+#tanstypage /
+number3 = 0
+print(bool (number3))
+
+#Jamais autre chose qu'un nombre dans la chaine de character si on veut la convertir en int
+text3 = "123456789 yoooo"
+print(int (text3))
+
+#Fonctions de transtypage
+#str converti vers string chaine de char
+#int convert vers integer / nbr entier
+#bool convert vers booléen
+#float convert vers un float
+
+
+
